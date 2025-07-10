da0021d03de4efb44446952e845155d40b775044
psi-cmd <psi-cmd@outlook.com>
2025-07-09 23:59:16 -0500

    finetune update CHTC script

--- CHTC_setup.sh
+++ CHTC_setup.sh
@@ -1,9 +1,10 @@
-export HOME=`pwd ..`/..
+export HOME=`pwd`/..
 export CPATH=/opt/cutlass/include:$CPATH
 export CPLUS_INCLUDE_PATH=/opt/cutlass/include:$CPLUS_INCLUDE_PATH
 export LD_LIBRARY_PATH=/opt/cutlass/lib:$LD_LIBRARY_PATH
 
 ln -s /workspace/data/ ./data
 ln -s /workspace/release_data/ ./release_data
+ln -s /staging/lliu466/torch_extensions/ ../.cache/torch_extensions
 
 pip install -e .
